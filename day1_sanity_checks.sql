5e3194a8e19336f779451ed51544c56ebe4c9593
Rakshith m <rakshithm118@gmail.com>
2026-01-14 23:31:45 +0530

    Add files via upload

---
+++ day1_sanity_checks.sql
@@ -0,0 +1,7 @@
+Day 1: Sanity checks
+
+SELECT COUNT(*) FROM sod_register;
+SELECT COUNT(*) FROM sod_session_start;
+SELECT COUNT(*) FROM sod_revenue;
+SELECT COUNT(*) FROM sod_adrevenue;
+SELECT COUNT(*) FROM sod_battle_short;
